f624284b3b37af4485338cb26153d3a09a452a70
Mary White <mary.a.white28@gmail.com>
2022-02-17 13:50:10 -0500

    initial file setup, and pom dependencies

---
+++ src/main/java/com/mchindwhite/backendtechnical/BackendtechnicalApplication.java
@@ -0,0 +1,13 @@
+package com.mchindwhite.backendtechnical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtechnicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtechnicalApplication.class, args);
+	}
+
+}
