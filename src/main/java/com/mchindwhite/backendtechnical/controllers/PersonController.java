206058d1b21f064c39eb8e5aa8791acebbfcf03e
Mary White <mary.a.white28@gmail.com>
2022-02-18 14:16:49 -0500

    added delete method and put method. Bug in PutMapping

--- src/main/java/com/mchindwhite/backendtechnical/controllers/PersonController.java
+++ src/main/java/com/mchindwhite/backendtechnical/controllers/PersonController.java
@@ -2,10 +2,11 @@ package com.mchindwhite.backendtechnical.controllers;
 
 import com.mchindwhite.backendtechnical.models.Person;
 import com.mchindwhite.backendtechnical.repositories.PersonRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -36,4 +37,28 @@ public class PersonController {
         return personDao.save(newPerson);
     }
 
+    //delete a person
+    @DeleteMapping(path = "{personId}")
+    public void deletePerson(@PathVariable("personId") Long id) {
+        boolean exists = personDao.existsById(id);
+        if (!exists) {
+            throw new IllegalStateException("person with id " + id + " does not exist");
+        }
+        personDao.deleteById(id);
+    }
+
+    //edit a person
+    @PutMapping(path = "{personId}")
+    public Person updatePerson(@PathVariable("personId") Long id, @RequestParam(required = false) String name) {
+        Person person = personDao.findById(id).orElseThrow(() -> new IllegalStateException("person with id " + id + " doesn't exist"));
+        System.out.println("line 54 " + name);
+        if (name != null && name.length() > 0 && !Objects.equals(person.getName(), name)) {
+            person.setName(name);
+            person.setDateUpdated(LocalDate.now());
+        }
+        return personDao.save(person);
+    }
+
 }
+
+
--- src/main/java/com/mchindwhite/backendtechnical/repositories/PersonRepository.java
+++ src/main/java/com/mchindwhite/backendtechnical/repositories/PersonRepository.java
@@ -13,4 +13,5 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     @Query("SELECT n FROM Person n WHERE n.name = ?1")
     Optional<Person> findPersonByName(String name);
 
+
 }
